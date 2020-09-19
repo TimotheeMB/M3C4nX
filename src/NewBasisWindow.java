4778af4bf9a4ccba9a7ed5b20478f3500f48b869
Timothée <timothee.me@gmail.com>
2020-09-19 18:46:57 +0200

    Visuel

--- src/NewBasisWindow.java
+++ src/NewBasisWindow.java
@@ -7,7 +7,8 @@ public class NewBasisWindow extends JFrame implements ActionListener {
     UI ui;
 
     public NewBasisWindow(UI ui) {
-        nbp = new NewBasisPanel(ui,this);
+        this.setTitle("New Basis");
+        this.nbp = new NewBasisPanel(ui,this);
         this.ui=ui;
         this.getContentPane().add(nbp);
         this.pack();
